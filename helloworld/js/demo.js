e8374df01709bb2bfb74b3f75cdec1f74a93797b
chen.wenjun <chen.wenjun@vhd.com.cn>
2023-02-06 01:28:13 +0800

    add js demo

---
+++ helloworld/js/demo.js
@@ -0,0 +1,21 @@
+/**  
+*auto create by /home/cwj/tool.py,according to doxygen 
+*/ 
+function alert_commit()
+{
+alert("提交完成 ")
+}
+
+
+function clear()
+{
+document.write("clear finish")
+}
+
+
+function print_date()
+{
+var time=new Date();
+var today=time.getFullYear() + "年" +(time.getMonth()+1) +"月"+time.getDate()+"日"
+alert(today);
+}
