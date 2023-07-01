82eade5108e8278caf80d58ce89ee5d6a6cfef5f
chen.wenjun <chen.wenjun@vhd.com.cn>
2023-07-01 16:59:28 +0800

    support get method

--- lighttpd_helloworld/www/cgi-bin/get_w.cpp
+++ lighttpd_helloworld/www/cgi-bin/get_w.cpp
@@ -23,27 +23,16 @@ int main()
      * http://192.168.1.191:8088/cgi-bin/get_w.cgi?keya=1&keyb=2
      * form的内容以明文的方式放在url中，用QUERY_STRING来获取内容，但是url有长度限制，所以只适用于少量数据，数据多的话使用post方法
     */
-   #if 0
-   const char* cookie_name = "my_cookie";
-    const char* cookie_value = "cookie_value";
-    
-    server *srv = server_init();
-    connection *con = connection_init(srv);
-    response_header_overwrite(srv, con, "Set-Cookie", cookie_name, cookie_value, strlen(cookie_value));
-    
-    // 其他处理响应的代码...
-    
-    connection_close(srv, con);
-    server_free(srv);
-   #else
+   
     char *form=getenv("QUERY_STRING");
     char keya[50]="",keyb[50]="";
     char buf[1024];
     printf("Content-Type:text/html;\r\n\r\n");
-    //printf("Set-Cookie: name='1';expires=Wed, 28 Aug 2024 18:30:00 GMT\n");
-
+    
+    #if 0
     printf("<html>");
     printf("<head>");
+    //printf("<meta http-equiv=Refresh content=1;URL=./showCookie.html\n"); 
     printf("<title>hide</title>");
 
     printf("</head>");
@@ -52,17 +41,18 @@ int main()
     printf("</body>");
 
     printf("</html>");
+    #endif
     if(NULL == form)
     {
         return 0;
     }
-    //printf("form:%s\n",form);
+    printf("form:%s\n",form);
     sscanf(form,"keya=%[^&]&keyb=%s",keya,keyb);
     
     FILE *fp=fopen("./data.conf","w");
     snprintf(buf,sizeof(buf)-1,"keya=%s\nkeyb=%s\n",keya,keyb);
     fwrite(buf,strlen(buf),1,fp);
     fclose(fp);
-    #endif
+    
 
 }
--- lighttpd_helloworld/www/cgi-bin/hello_world.cpp
+++ lighttpd_helloworld/www/cgi-bin/hello_world.cpp
@@ -15,14 +15,25 @@
 #include <iostream>
 using namespace std;;
 
-
+const string ENV[ 24 ] = {                 
+        "COMSPEC", "DOCUMENT_ROOT", "GATEWAY_INTERFACE",   
+        "HTTP_ACCEPT", "HTTP_ACCEPT_ENCODING",             
+        "HTTP_ACCEPT_LANGUAGE", "HTTP_CONNECTION",         
+        "HTTP_HOST", "HTTP_USER_AGENT", "PATH",            
+        "QUERY_STRING", "REMOTE_ADDR", "REMOTE_PORT",      
+        "REQUEST_METHOD", "REQUEST_URI", "SCRIPT_FILENAME",
+        "SCRIPT_NAME", "SERVER_ADDR", "SERVER_ADMIN",      
+        "SERVER_NAME","SERVER_PORT","SERVER_PROTOCOL",     
+        "SERVER_SIGNATURE","SERVER_SOFTWARE" }; 
 
 int main()
 {
     /**
      * https://blog.csdn.net/Alright_Well/article/details/68486072
      * 注意下划线
+     * https://www.runoob.com/cplusplus/cpp-web-programming.html
      */
+    #if 0
     printf("Content-Type:text/html;\r\n\r\n");
     printf("hello world\n");
 
@@ -50,5 +61,32 @@ int main()
     printf("CONTENT_TYPE %s\n",getenv("CONTENT_TYPE"));printf("<br>");
     printf("CONTENT_LENGTH %s\n",getenv("CONTENT_LENGTH"));printf("<br>");
     printf("DOMAIN_NAME %s\n",getenv("DOMAIN_NAME"));printf("<br>");
+    #else
+    cout << "Content-type:text/html\r\n\r\n";
+   cout << "<html>\n";
+   cout << "<head>\n";
+   cout << "<title>CGI 环境变量</title>\n";
+   cout << "</head>\n";
+   cout << "<body>\n";
+   cout << "<table border = \"0\" cellspacing = \"2\">";
+ 
+   for ( int i = 0; i < 24; i++ )
+   {
+       cout << "<tr><td>" << ENV[ i ] << "</td><td>";
+       // 尝试检索环境变量的值
+       char *value = getenv( ENV[ i ].c_str() );  
+       if ( value != 0 ){
+         cout << value;                                 
+       }else{
+         cout << "环境变量不存在。";
+       }
+       cout << "</td></tr>\n";
+   }
+   cout << "</table><\n";
+   cout << "</body>\n";
+   cout << "</html>\n";
+   
+   return 0;
+    #endif
     
 }
