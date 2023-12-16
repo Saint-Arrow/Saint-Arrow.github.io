c29e2752774d262a7135f686cf0b3bc49197e283
copy_study <924698880@qq.com>
2023-12-16 22:31:49 +0800

    支持首页点击导航栏再显示对应的内容，通过id和display来实现

---
+++ js/demo.js
@@ -0,0 +1,29 @@
+
+/* js的注释比C语言多一个*号，注释快捷键为alt+shift+A */
+
+window.onload =function(){
+
+// 获取所有的导航标签和内容段落
+var tabs = document.querySelectorAll("div.left-column ul li a");
+var contents = document.querySelectorAll('.right-column');
+
+// 添加事件监听器
+for (var i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener('click', function(e) {
+    e.preventDefault(); // 阻止默认行为
+
+    // 隐藏所有的内容段落
+    for (var j = 0; j < contents.length; j++) {
+      contents[j].style.display = 'none';
+    }
+
+    // 显示当前导航标签对应的内容段落
+    var targetId = this.getAttribute('id').replace('tab', '');
+    var targetContent = document.getElementById(targetId);
+    targetContent.style.display = 'block';
+  });
+};
+
+
+
+};
